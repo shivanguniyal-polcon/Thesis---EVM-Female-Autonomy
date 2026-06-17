56e5a0f5668a5edaf43789b1fed7823009328b4d
Shivang Uniyal <shivang.uniyal27@gmail.com>
2026-06-17 18:02:18 +0530

    Update main.py

--- projects/project_B/main.py
+++ projects/project_B/main.py
@@ -1,3 +1,15 @@
+# EVM & Female Autonomy Analytical Pipeline
+# Copyright (C) 2026 Shivang Uniyal
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+
 import pandas as pd
 import geopandas as gpd
 import json
